15a25ac328b49e9909d13307f841b0bfc7c69e34
milagros alvarez <milagrosalvarez2604@gmail.com>
2025-08-27 22:32:21 -0300

    add english default

--- magno-website/src/app/page.tsx
+++ magno-website/src/app/page.tsx
@@ -2,5 +2,5 @@ import {redirect} from 'next/navigation';
  
 // Redirect the user to the default locale when `/` is requested
 export default function RootPage() {
-  redirect('/es');
+  redirect('/en');
 }
