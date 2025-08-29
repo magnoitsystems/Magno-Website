161c9f4e3372499f56b8d4ea599d878f883aa859
Ana Bellinzona <anabellinzona@gmail.com>
2025-08-29 14:52:12 -0300

    add form mobile

--- magno-website/src/components/form/form.tsx
+++ magno-website/src/components/form/form.tsx
@@ -3,13 +3,15 @@ import styles from './form.module.css';
 export default function Form(){
     return (
         <form className={styles.formProperties}>
-            <label>Name and lastname <input type="text" placeholder="Name and lastname" name="nameAndLastname" id="nameAndLastname"/></label>
-            <label>What sector is the site geared towards? <input type="text" placeholder="What sector is the site geared towards?" name="sector" id="sector"/></label>
-            <label>Name and lastname <input type="text" placeholder="Name and lastname" name="nameAndLastname" id="nameAndLastname"/></label>
-            <label>Does it need auto-administration? <select name="autoadministration">
-                <option id="yes">Yes</option>
-                <option id="no">No</option>
-            </select></label>
+            <div className={styles.labelsProperties}>
+                <label>Name and lastname <input type="text" placeholder="Name and lastname" name="nameAndLastname" id="nameAndLastname"/></label>
+                <label>What sector is the site geared towards? <input type="text" placeholder="What sector is the site geared towards?" name="sector" id="sector"/></label>
+                <label>Name and lastname <input type="text" placeholder="Name and lastname" name="nameAndLastname" id="nameAndLastname"/></label>
+                <label>Does it need auto-administration? <select name="autoadministration">
+                    <option id="yes">Yes</option>
+                    <option id="no">No</option>
+                </select></label>
+            </div>
             <button className={styles.buttonProperties}>Solicitar presupuesto</button>
         </form>
     );
