38d752f8ffc7aa09fe5eb8ade2699957f86d4f1e
Agostina Bidegain <agostinabidegain@gmail.com>
2025-08-30 09:10:42 -0300

    retouch some details

--- magno-website/src/app/[locale]/components/form/form.tsx
+++ magno-website/src/app/[locale]/components/form/form.tsx
@@ -1,8 +1,9 @@
 import styles from './form.module.css';
+import {inter} from "@/app/ui/fonts";
 
 export default function Form(){
     return (
-        <form className={styles.formProperties}>
+        <form className={`${inter.className} ${styles.formProperties}`}>
             <div className={styles.labelsProperties}>
                 <label>Name and lastname <input type="text" placeholder="Name and lastname" name="nameAndLastname" id="nameAndLastname"/></label>
                 <label>What sector is the site geared towards? <input type="text" placeholder="What sector is the site geared towards?" name="sector" id="sector"/></label>
