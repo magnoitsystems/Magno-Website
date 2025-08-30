f531df6b8c9b8b032245a440bc866d7e9e86d5f8
Agostina Bidegain <agostinabidegain@gmail.com>
2025-08-30 09:24:08 -0300

    added magno background

--- magno-website/src/app/[locale]/page.tsx
+++ magno-website/src/app/[locale]/page.tsx
@@ -2,16 +2,21 @@ import Services from "./components/servicesSection/services";
 import NavBar from "@/app/[locale]/components/navBar/navBar";
 import PortfolioProjects from "./components/portfolio/portfolioProjects";
 import WelcomeSection from "@/app/[locale]/components/welcomeSection/welcomeSection";
+import styles from './page.module.css';
 
 export default function HomePage() {
   return (
-    <main>
+    <main className={styles.main}>
         <div>
             <NavBar/>
         </div>
+
         <WelcomeSection/>
-       <Services></Services>
-       <PortfolioProjects></PortfolioProjects>
+
+        <div className={styles.logoContainer}>
+            <Services></Services>
+            <PortfolioProjects></PortfolioProjects>
+        </div>
     </main>
   );
 }
